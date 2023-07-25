72a5f9e229e156f92da4c49169ce5ad175b05483
Bartosz Kukuła <bartosz.kukula@firma.interia.pl>
2023-07-25 20:21:13 +0200

    Complete app

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
 };
 
 module.exports = config;
